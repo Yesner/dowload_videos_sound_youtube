e0a5a4945272c93716a1e42e454a1f9d8afb0a3d
yesner <yesnersalgado@gmail.com>
2025-01-13 12:34:46 -0600

    updated code

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 import streamlit as st
 from pytube.__main__ import YouTube
+from pytube.exceptions import PytubeError
+
 st.title("Youtube Video Downloader")
 st.subheader("Enter the URL:")
 url = st.text_input(label='URL')
+
 if url != '':
-    yt = YouTube(url)
-    st.image(yt.thumbnail_url, width=300)
-    st.subheader('''
-    {}
-    ## Length: {} seconds
-    ## Rating: {} 
-    '''.format(yt.title , yt.length , yt.rating))
-    video = yt.streams
-    if len(video) > 0:
-        downloaded , download_audio = False , False
-        download_video = st.button("Download Video")
-        if yt.streams.filter(only_audio=True):
-            download_audio = st.button("Download Audio Only")
-        if download_video:
-            video.get_lowest_resolution().download()
-            downloaded = True
-        if download_audio:
-            video.filter(only_audio=True).first().download()
-            downloaded = True
-        if downloaded:
-            st.subheader("Download Complete")
-            import subprocess
-            subprocess.run(["ls"])
-    else:
-        st.subheader("Sorry, this video can not be downloaded")
+    try:
+        yt = YouTube(url)
+        st.image(yt.thumbnail_url, width=300)
+        st.subheader('''
+        {}
+        ## Length: {} seconds
+        ## Rating: {} 
+        '''.format(yt.title, yt.length, yt.rating))
+        video = yt.streams
+        if len(video) > 0:
+            downloaded, download_audio = False, False
+            download_video = st.button("Download Video")
+            if yt.streams.filter(only_audio=True):
+                download_audio = st.button("Download Audio Only")
+            if download_video:
+                video.get_lowest_resolution().download()
+                downloaded = True
+            if download_audio:
+                video.filter(only_audio=True).first().download()
+                downloaded = True
+            if downloaded:
+                st.subheader("Download Complete")
+                import subprocess
+                subprocess.run(["ls"])
+        else:
+            st.subheader("Sorry, this video can not be downloaded")
+    except PytubeError as e:
+        st.error(f"An error occurred: {e}")
